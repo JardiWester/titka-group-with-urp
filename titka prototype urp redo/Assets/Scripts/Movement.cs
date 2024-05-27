1f48759a8e1cb4ef931485720fac3046441f501d
OzanNail <ozan.nail3232@gmail.com>
2024-05-27 18:58:56 +0200

    run animation / feature,

--- titka prototype urp redo/Assets/Scripts/Movement.cs
+++ titka prototype urp redo/Assets/Scripts/Movement.cs
@@ -8,7 +8,8 @@ public class Movement : MonoBehaviour
     float turnSmoothVelocity;
 
     [SerializeField] private float gravity = -9.8f;
-    [SerializeField] private float speed = 6f;
+    [SerializeField] private float speed = 8f;
+    [SerializeField] private float sprintSpeed = 13f; // Added sprint speed
     [SerializeField] private float jumpHeight = 3f;
     Vector3 velocity;
     bool isGrounded;
@@ -18,23 +19,21 @@ public class Movement : MonoBehaviour
     public LayerMask groundMask;
     public Animator playerAnim;
 
-void Start()
-{
-    Cursor.visible = false;
-    Cursor.lockState = CursorLockMode.Locked;
-    
-}
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        //walk
-         
+        // Walk
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-        //jump
+        // Jump
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
         if (isGrounded && velocity.y < 0)
@@ -46,29 +45,40 @@ void Start()
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
         }
-        //gravity
-        velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
 
+        // Gravity
+        //velocity.y += gravity * Time.deltaTime;
+        //controller.Move(velocity * Time.deltaTime);
 
+        // Check if sprinting
+        bool isSprinting = Input.GetKey(KeyCode.LeftShift);
+
+        // Calculate speed
+        float currentSpeed = isSprinting ? sprintSpeed : speed;
 
         if (direction.magnitude >= 0.1f)
         {
             playerAnim.SetBool("AnimWalk", true);
-           
+            playerAnim.SetBool("RunAnim", false);
+            if (isSprinting)
+            {
+                playerAnim.SetBool("AnimWalk", false);
+
+                playerAnim.SetBool("RunAnim", true);
+            }
+            
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+            controller.Move(moveDir.normalized * currentSpeed * Time.deltaTime);
         }
         else
         {
             playerAnim.SetBool("AnimWalk", false);
-            
+            playerAnim.SetBool("RunAnim", false);
+            Debug.Log("stoop walk");
         }
-
-        
     }
 }
